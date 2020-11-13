40dc3b9d9c69b24b6b1ad423317bb22cf19886ef
Jensen Lloyd <jl.110@protonmail.com>
2020-11-13 23:03:41 +1100

    Getting all the data from API

--- data_scraper.py
+++ data_scraper.py
@@ -17,7 +17,53 @@ time = str(datetime.now().time())[:5]
 #     time = str(datetime.now().time())[:5]
 
   
+data = requests.get('https://api.binance.com/api/v3/ticker/24hr', {"symbol": "BTCAUD"}).json()
 
-# buy_price = requests.get('https://api.binance.com/api/v3/ticker/24hr', {"symbol": "BTCAUD"}).json()
-data = json.dumps(requests.get('https://api.binance.com/api/v3/ticker/24hr', {"symbol": "BTCAUD"}).json(), indent=4)
-print(data)
+raw_price_change = data["priceChange"]
+print(raw_price_change)
+
+price_change_percent = data["priceChangePercent"]
+print(price_change_percent)
+
+weighted_avg = data["weightedAvgPrice"]
+print(weighted_avg)
+
+last_price = data["lastPrice"]
+print(last_price)
+
+last_qty = data["lastQty"]
+print(last_qty)
+
+bid_price = data["bidPrice"]
+print(bid_price)
+
+bid_qty = data["bidQty"]
+print(bid_qty)
+
+ask_price = data["askPrice"]
+print(last_price)
+
+ask_qty = data["askQty"]
+print(ask_qty)
+
+open_price = data["openPrice"]
+print(open_price)
+
+high_price = data["highPrice"]
+print(high_price)
+
+low_price = data["lowPrice"]
+print(low_price)
+
+trading_volume = data["volume"]
+print(trading_volume)
+
+quote_volume = data["quoteVolume"]
+print(quote_volume)
+
+
+
+# previous_buy = 
+# print(previous_buy)
+# previous_sell =
+# print(previous_sell) 
