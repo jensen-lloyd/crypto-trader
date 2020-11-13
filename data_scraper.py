fe9f064a04eb87af707095bb7ccb3934a87b8e13
Jensen Lloyd <jl.110@protonmail.com>
2020-11-13 23:29:53 +1100

    Some small changes, trying to fix error where if statement is ignored

--- data_scraper.py
+++ data_scraper.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from datetime import datetime
+from time import sleep
 
 try:
     requests.get('https://api.binance.com/api/v3/ping').json()
@@ -10,60 +11,69 @@ except:
 time = str(datetime.now().time())[:5]
 
 
+def get_data():
+
+    time = str(datetime.now().time())[:5]
+
+    # previous_buy_price = bid_price
+    # print(previous_buy)
+
+    # previous_sell_price = ask_price
+    # print(previous_sell)
+
+    data = requests.get('https://api.binance.com/api/v3/ticker/24hr', {"symbol": "BTCAUD"}).json()
+
+    raw_price_change = data["priceChange"]
+    print(raw_price_change)
+
+    price_change_percent = data["priceChangePercent"]
+    print(price_change_percent)
+
+    weighted_avg = data["weightedAvgPrice"]
+    print(weighted_avg)
+
+    last_price = data["lastPrice"]
+    print(last_price)
+
+    last_qty = data["lastQty"]
+    print(last_qty)
+
+    bid_price = data["bidPrice"]
+    print(bid_price)
+
+    bid_qty = data["bidQty"]
+    print(bid_qty)
+
+    ask_price = data["askPrice"]
+    print(last_price)
+
+    ask_qty = data["askQty"]
+    print(ask_qty)
+
+    open_price = data["openPrice"]
+    print(open_price)
+
+    high_price = data["highPrice"]
+    print(high_price)
+
+    low_price = data["lowPrice"]
+    print(low_price)
+
+    trading_volume = data["volume"]
+    print(trading_volume)
+
+    quote_volume = data["quoteVolume"]
+    print(quote_volume) 
+
+    # sleep(299)
+    
+    if time == "6:58":
+        exit()
 
 
 
-# while time != 6:58:
-#     time = str(datetime.now().time())[:5]
 
-  
-data = requests.get('https://api.binance.com/api/v3/ticker/24hr', {"symbol": "BTCAUD"}).json()
+time = str(datetime.now().time())[:5]
 
-raw_price_change = data["priceChange"]
-print(raw_price_change)
-
-price_change_percent = data["priceChangePercent"]
-print(price_change_percent)
-
-weighted_avg = data["weightedAvgPrice"]
-print(weighted_avg)
-
-last_price = data["lastPrice"]
-print(last_price)
-
-last_qty = data["lastQty"]
-print(last_qty)
-
-bid_price = data["bidPrice"]
-print(bid_price)
-
-bid_qty = data["bidQty"]
-print(bid_qty)
-
-ask_price = data["askPrice"]
-print(last_price)
-
-ask_qty = data["askQty"]
-print(ask_qty)
-
-open_price = data["openPrice"]
-print(open_price)
-
-high_price = data["highPrice"]
-print(high_price)
-
-low_price = data["lowPrice"]
-print(low_price)
-
-trading_volume = data["volume"]
-print(trading_volume)
-
-quote_volume = data["quoteVolume"]
-print(quote_volume)
-
-
-
-# previous_buy = 
-# print(previous_buy)
-# previous_sell =
-# print(previous_sell) 
+if time[3:] == "0" or "5":
+        get_data()
